fb0c05e14d361e670ba4c95e52b34800e79be216
shubh <shahshubh1010@gmail.com>
2021-12-09 20:18:31 +0530

    fix: ignore NaN in numbers

--- average.js
+++ average.js
@@ -1,6 +1,14 @@
 
 function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  var sum = 0;
+  var count = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    if (!isNaN(numbers[i])) {
+      sum += numbers[i];
+      count++;
+    }
+  }
+  return sum / count;
 }
 
 module.exports = {average};
