900529baa7d2710f247aab7843f4380a19c9fde2
shubh <shahshubh1010@gmail.com>
2021-12-16 13:05:03 +0530

    feat: added functionality of checking for faulty sensors

--- average.js
+++ average.js
@@ -1,7 +1,32 @@
-
 function average(numbers) {
-  const filteredNumbers = numbers.filter((number) => !isNaN(number));
-  return filteredNumbers.reduce((p, c)=> p + c, 0) / filteredNumbers.length;
+  const PERCENTAGE_THRESHOLD = 0.3;
+  const CONSECUTIVE_NAN_THRESHOLD = Math.floor(Math.sqrt(numbers.length));
+  
+  let nanCount = 0;
+  let numbersSum = 0;
+  let consecutiveNanCount = 0;
+  let maxConsecutiveNanCount = 0;
+  
+  numbers.forEach((number) => {
+    if (isNaN(number)) {
+      nanCount++;
+      consecutiveNanCount++;
+    } else {
+      numbersSum += number;
+      consecutiveNanCount = 0;
+    }
+    maxConsecutiveNanCount = Math.max(maxConsecutiveNanCount, consecutiveNanCount);
+  });
+
+  // Check 1: If percentage of NaN is greater than 30%, return NaN indicating sensors are faulty.
+  if(nanCount / numbers.length > PERCENTAGE_THRESHOLD) return NaN;
+
+  // Check 2: If length of consecutive NaN is greater than THRESHOLD, return NaN indicating sensors are faulty.
+  // THRESHOLD = sqrt(length of numbers array).
+  if(maxConsecutiveNanCount > CONSECUTIVE_NAN_THRESHOLD) return NaN;
+
+  // else return average of numbers.
+  return numbersSum / (numbers.length - nanCount);
 }
 
 module.exports = {average};
--- test/average.test.js
+++ test/average.test.js
@@ -12,5 +12,17 @@ it('reports the average as NaN on an empty list', ()=> {
 });
 
 it('ignores NaN in the input', ()=> {
-  expect(average([1, NaN, 2])).to.be.approximately(1.5, 0.01);
+  expect(average([1, NaN, 2, 3, 4])).to.be.approximately(2.5, 0.01);
+});
+
+it('reports the average as NaN if most readings are NaN indicating some fault in sensor', ()=> {
+  expect(average([1, NaN, 2, NaN, 3, 4, NaN, NaN, NaN])).to.be.NaN;
+});
+
+it('reports the average as NaN if there is long consecutive sequence of NaN readings in the input', ()=> {
+  expect(average([1, 2, NaN, NaN, NaN, NaN, NaN, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])).to.be.NaN;
+});
+
+it('computes the average, if less than 30% NaN and no long consecutive sequence of NaN readings are there in the input', ()=> {
+  expect(average([1, NaN, NaN, 2, 3, 4, 5, NaN, 6, 7, 8, NaN, 9, 10, NaN, 11, 12])).to.be.approximately(6.5, 0.01);
 });
